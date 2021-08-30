1cd81062660e3d93c21517bc6ddc96abf1625587
ErickDS <erycdiogenes@gmail.com>
2021-08-30 11:19:44 -0300

    Primeiro commit do jogo da velha simples

---
+++ src/jogodavelha/Jogador.java
@@ -0,0 +1,10 @@
+package jogodavelha;
+
+import java.io.Serializable;
+
+
+public class Jogador implements Serializable {
+    public String nome;
+    public int vitorias;
+    public int derrotas;
+}
